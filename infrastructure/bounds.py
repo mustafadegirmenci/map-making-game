95a8330dd44f7bb4da3e85f3c61cfae6060374d2
mustafadegirmenci <m.dgr@yahoo.com>
2023-12-28 23:32:00 +0300

    Add bound iteration

--- infrastructure/bounds.py
+++ infrastructure/bounds.py
@@ -1,9 +1,14 @@
 class Bounds:
-    def __init__(self, min_x: float, max_x: float, min_y: float, max_y: float):
+    def __init__(self, min_x: int, max_x: int, min_y: int, max_y: int):
         self.min_x = min_x
         self.max_x = max_x
         self.min_y = min_y
         self.max_y = max_y
 
-    def is_within_bounds(self, x: float, y: float) -> bool:
+    def is_within_bounds(self, x: int, y: int) -> bool:
         return self.min_x <= x <= self.max_x and self.min_y <= y <= self.max_y
+
+    def iterate_through_points(self, func):
+        for x in range(self.min_x, self.max_x + 1):
+            for y in range(self.min_y, self.max_y + 1):
+                func(x, y)
